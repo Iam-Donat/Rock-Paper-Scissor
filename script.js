b170bfcd608c514f3891c04e56fb75d0c9df9c0c
Iam-Ntwali <64748718+Iam-Ntwali@users.noreply.github.com>
2023-07-21 16:20:19 +0200

    some changes 💀😂😂

--- script.js
+++ script.js
@@ -6,14 +6,13 @@ let scoreComputerDisplay = document.querySelector(".scoreComputer");
 
 let playerChoiceDisplay = document.querySelector(".playerChoice");
 let computerChoiceDisplay = document.querySelector(".computerChoice");
-let message = document.querySelector("#message");
+let message = document.querySelector(".message");
 
 const overlay = document.querySelector(".overlay");
 const modal = document.querySelector(".modal"); //Winner Announcement 
 const winner = document.querySelector(".winnerDisplay");
 
 const restartButton = document.querySelector("#restartBtn"); // Reset the game
-
 
 //Initializing Score Value
 let playerScore = 0;  // Initial Player scores
@@ -22,59 +21,35 @@ let computerScore = 0;  // Initial Computer scores
 let playerMove = ""; // Get Player Choice to trigger the gameplay
 
 function getComputerChoice() {
-
     // Getting the Computer Choices
     const choice = ["Rock", "Paper", "Scissors"]
 
     //Getting a Random Index to match the computer choices
     const randomChoice = Math.floor(Math.random() * choice.length);
 
-    // console.log(randomChoice)
-
     const playMove = choice[randomChoice];
-
-    // console.log(playMove)
     return playMove;
 }
 
 // Check winner of each round
+function updateDisplay(playerSelection, computerSelection, messageText) {
+    scorePlayerDisplay.textContent = playerScore;
+    scoreComputerDisplay.textContent = computerScore;
+    playerChoiceDisplay.textContent = `${playerSelection}`;
+    computerChoiceDisplay.textContent = `${computerSelection}`;
+    message.textContent = messageText;
+}
+
 function playRound(playerSelection, computerSelection) {
-
     if (playerSelection === computerSelection) {
-
-        scorePlayerDisplay.textContent = playerScore;
-        scoreComputerDisplay.textContent = computerScore;
-        // console.log(playerScore);
-        // console.log(computerScore);
-
-        playerChoiceDisplay.textContent = `${playerSelection}`;
-        computerChoiceDisplay.textContent = `${computerSelection}`;
-
-        message.textContent = `It's a tie!🫱🏾‍🫲🏾`;
-
+        updateDisplay(playerSelection, computerSelection, `It's a tie!🫱🏾‍🫲🏾`);
     } else if ((playerSelection === 'Rock' && computerSelection === 'Scissors') || (playerSelection === 'Scissors' && computerSelection === 'Paper') || (playerSelection === 'Paper' && computerSelection === 'Rock')) {
-
         playerScore++
-        // console.log('Player Score: ' + playerScore);
-        scorePlayerDisplay.textContent = playerScore;
-
-        playerChoiceDisplay.textContent = `${playerSelection}`;
-        computerChoiceDisplay.textContent = `${computerSelection}`;
-
-        message.textContent = `You Win 🥇! ${playerSelection} beats ${computerSelection}!`;
-
+        updateDisplay(playerSelection, computerSelection, `You Win 🥇! ${playerSelection} beats ${computerSelection}!`);
     } else {
-
         computerScore++
-        scoreComputerDisplay.textContent = computerScore;
-
-        playerChoiceDisplay.textContent = `${playerSelection}`;
-        computerChoiceDisplay.textContent = `${computerSelection}`;
-
-        message.textContent = `You lose 🥲! ${computerSelection} beats ${playerSelection}!`;
-
+        updateDisplay(playerSelection, computerSelection, `You lose 🥲! ${computerSelection} beats ${playerSelection}!`);
     }
-
 }
 
 function announceTheWinner() {
@@ -83,36 +58,44 @@ function announceTheWinner() {
         restartButton.classList.add('loser');
         modal.classList.remove('display');
         overlay.classList.remove('display');
-
         winner.textContent = `Sorry you lost the game🥲!! with a score of ${playerScore} against computer's score of ${computerScore}!`;
-        restartButton.innerHTML = `Try again!!`;
+        restartButton.textContent = `Try again!!`;
     } else {
         modal.classList.add('winner');
         restartButton.classList.add('winner');
         modal.classList.remove('display');
         overlay.classList.remove('display');
-
         winner.textContent = `You've won!🎇🥳🥂 with a score of ${playerScore} against computer's score of ${computerScore}!`;
-
+        restartButton.textContent = `Play Again!!`;
     }
 }
 
-function game() {
-
-    const playerSelection = playerMove;
-    const computerSelection = getComputerChoice();
-
-    playRound(playerSelection, computerSelection);
-
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    scorePlayerDisplay.textContent = 0;
+    scoreComputerDisplay.textContent = 0;
+    playerChoiceDisplay.textContent = '';
+    computerChoiceDisplay.textContent = '';
+    message.textContent = '';
+    winner.textContent = '';
+    modal.classList.remove('winner', 'loser');
+    restartButton.classList.remove('winner');
+    restartButton.classList.remove('loser');
+    overlay.classList.add('display');
+    modal.classList.add('display');
 }
 
+function game() {
+    const playerSelection = playerMove;
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+}
 
 movesBtn.forEach(btn => {
     btn.addEventListener("click", () => {
-
         playerMove = btn.getAttribute("data-value");
-
-        if ((playerScore === 5) || (computerScore === 5)) {
+        if ((playerScore > 4) || (computerScore > 4)) {
             announceTheWinner();
         } else {
             game();
@@ -120,4 +103,4 @@ movesBtn.forEach(btn => {
     });
 });
 
-restartButton.addEventListener("click", () => { window.location.reload() });
+restartButton.addEventListener("click", resetGame);
