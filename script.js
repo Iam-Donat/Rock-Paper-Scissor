8d0adc0d5749287057c07d0305ac1c799c107f57
Iam-Ntwali <64748718+Iam-Ntwali@users.noreply.github.com>
2023-09-19 11:04:24 +0200

    some changes

--- script.js
+++ script.js
@@ -1,6 +1,55 @@
 // === Get HTLM Elements === //
 const overlay = document.querySelector('.overlay');
 
+const scoreDisplayPlayer = document.querySelector('.player__score');
+const scoreDisplayComputer = document.querySelector('.computer__score');
+
+const playerMoves = document.querySelectorAll('.moves span');
+console.log(playerMoves);
+
+// Iniate values
+let player;
+let computer;
+
+let currentRoundNumber = 1;
+
+// functions
+function getPlayerMove() {
+    playerMoves.forEach(move => {
+        move.addEventListener('click', () => {
+            if (move.className === 'rock') {
+                player = 'rock';
+            } else if (move.className === 'paper') {
+                player = 'paper'
+            } else if (move.className === 'scissor') {
+                player = 'scissor'
+            };
+            return player
+        })
+    })
+
+}
+
+function getComputerMove() {
+    const choices = ['rock', 'paper', 'scissor'];
+
+    const randomChoice = Math.floor((Math.random() * choices.length));
+
+    computer = choices[randomChoice]
+
+    return computer;
+}
+
+function playerSelection(player, computer) {
+
+}
+
+
+
+
+
+
+
 // === Modal How to PLay 
 const helpBtn = document.querySelector('.help-btn');
 
