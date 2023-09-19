24a8f7de8cdf6f9b1c9a5d4ca334359f116070fe
Iam-Ntwali <64748718+Iam-Ntwali@users.noreply.github.com>
2023-09-19 13:13:43 +0200

    game almost done

--- script.js
+++ script.js
@@ -7,49 +7,61 @@ const scoreDisplayComputer = document.querySelector('.computer__score');
 const playerMoves = document.querySelectorAll('.moves span');
 console.log(playerMoves);
 
+const resultDisplay = document.querySelector('.result');
+const moveResult = document.querySelector('.state')
+const playerMove = document.querySelector('.player__move');
+const computerMove = document.querySelector('.computer__move');
+
 // Iniate values
-let player;
-let computer;
+let scorePlayer = 0;
+let scoreComputer = 0;
+let round = 1;
 
-let currentRoundNumber = 1;
-
+const choices = ['rock', 'paper', 'scissor'];
 // functions
-function getPlayerMove() {
-    playerMoves.forEach(move => {
-        move.addEventListener('click', () => {
-            if (move.className === 'rock') {
-                player = 'rock';
-            } else if (move.className === 'paper') {
-                player = 'paper'
-            } else if (move.className === 'scissor') {
-                player = 'scissor'
-            };
-            return player
-        })
-    })
-
-}
-
-function getComputerMove() {
-    const choices = ['rock', 'paper', 'scissor'];
-
+function getComputerMove() { //getting random computer move
     const randomChoice = Math.floor((Math.random() * choices.length));
-
-    computer = choices[randomChoice]
-
+    const computer = choices[randomChoice]
     return computer;
 }
 
-function playerSelection(player, computer) {
+function playRound(player) { //dictate the outcome of each round
+    const computerchoice = getComputerMove();
+    const roundResult = playersSelection(player, computerchoice);
+    resultDisplay.textContent = roundResult;
+    if (roundResult === `It's a tie!`) {
+        moveResult.textContent = ``
+    } else if (roundResult === `You win!`) {
+        moveResult.textContent = `${player} beats ${computerchoice}`;
+    } else {
+        moveResult.textContent = `${computerchoice} beats ${player}`;
+    }
 
 }
+function playersSelection(player, computer) {
+    if (player === computer) {
+        return `It's a tie!`;
+    } else if (
+        (player === 'rock' && computer === 'scissor') ||
+        (player === 'paper' && computer === 'rock') ||
+        (player === 'scissor' && computer === 'paper')
+    ) {
+        scoreDisplayPlayer.textContent = scorePlayer++
+        return `You win!`;
+    } else {
+        scoreDisplayComputer.textContent = scoreComputer++
+        return `Computer wins!`;
+    }
+}
 
-
-
-
-
-
-
+//EventListener to Trigger gameplay 
+playerMoves.forEach(move => {
+    move.addEventListener('click', () => {
+        const player = move.className
+        playRound(player)
+    })
+})
+//Modals
 // === Modal How to PLay 
 const helpBtn = document.querySelector('.help-btn');
 
